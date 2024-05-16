905fcf66f75761b660033e4eb2d9c0bf1c5f42fa
TizianoHranov <Tiziano.Hranov@copadata.com>
2024-05-16 14:24:54 +0200

    another failure

--- main.c
+++ main.c
@@ -6,13 +6,14 @@ uint8_t start = 0x01;
 uint8_t end = 0x00;
 uint8_t chan = 0x00;
 
+int readADC(uint8_t chan);
+float volts_adc(int adc);
+
 int readADC(uint8_t chan){
-    char buf[] = {start, (0x08|chan)<<4,end};
-    char readBuf[3];
-    printf("transfering now!\n");
-    bcm2835_spi_transfernb(buf,readBuf,3);
-    printf("transfer finished!\n");
-    return ((int)readBuf[1] & 0x03) << 8 | (int) readBuf[2];
+  char buf[] = {start, (0x08|chan)<<4,end};
+  char readBuf[3];
+  bcm2835_spi_transfernb(buf,readBuf,3);
+  return ((int)readBuf[1] & 0x03) << 8 | (int) readBuf[2];
 }
 
 float volts_adc(int adc) {
@@ -31,21 +32,24 @@ int main(int argc, char const *argv[])
     int dimensions_game[2] = {8, 32};
 
     game_init(&game, ball_xy, player1_xyw, player2_xyw, RIGHT, DOWN, 3, dimensions_game);
-    if (!bcm2835_init()){
-        printf("bcm2835_init failed. Are you running as root??\n");
-        return 1;
+    if (!bcm2835_init())
+    {
+      printf("bcm2835_init failed. Are you running as root??\n");
+      return 1;
     }
 
-    if (!bcm2835_spi_begin()){
-        printf("bcm2835_spi_begin failed. Are you running as root??\n");
-        return 1;
+    if (!bcm2835_spi_begin())
+    {
+      printf("bcm2835_spi_begin failed. Are you running as root??\n");
+      return 1;
     }
-
     bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
     bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128); // The default
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
     bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+
+    
     
     while(run){
         input = getc(stdin);
@@ -55,10 +59,10 @@ int main(int argc, char const *argv[])
                 break;
             case 'a':
                 printf("here we go:\n");
-                printf("volts[0]: %f\n", volts_adc(readADC(0)));
-                printf("volts[1]: %f\n\n", volts_adc(readADC(0)));
-                
-                
+                for(uint8_t i=0; i<10; i++) {
+                    int adc = readADC(chan);
+                    printf("ADC level on channel %02x is: %04x (%0.2f v)\n",chan,adc,volts_adc(adc));
+                }
 
                 break;
             case 'f':
