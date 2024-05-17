dc784ef4ceb944cc0a2c7ccf42c5fa22b4d24abd
TizianoHranov <Tiziano.Hranov@copadata.com>
2024-05-17 17:57:33 +0200

    k worked, now to another test

--- luma.led_matrix-master/examples/matrix_demo.py
+++ luma.led_matrix-master/examples/matrix_demo.py
@@ -22,7 +22,7 @@ def demo(n, block_orientation, rotate, inreverse):
 
     device = max7219(serial, cascaded=4, block_orientation=90, , blocks_arranged_in_reverse_order=True)
 
-    print("Created device")
+    print("displaying test bitmap now")
 
     MY_CUSTOM_BITMAP_FONT = [
         [
@@ -37,87 +37,7 @@ def demo(n, block_orientation, rotate, inreverse):
         # Note that "\0" is the zero-th character in the font (i.e the only one)
         legacy.text(draw, (0, 0), "\0", fill="white", font=MY_CUSTOM_BITMAP_FONT)
 
-    time.sleep(5000)
-
-    print("done with legacy.text")
-
-    msg = "Fast scrolling: Lorem ipsum dolor sit amet, consectetur adipiscing\
-    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut\
-    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in\
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint\
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit\
-    anim id est laborum."
-    msg = re.sub(" +", " ", msg)
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0)
-
-    msg = "Slow scrolling: The quick brown fox jumps over the lazy dog"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
-
-    print("Vertical scrolling")
-    words = [
-        "Victor", "Echo", "Romeo", "Tango", "India", "Charlie", "Alpha",
-        "Lima", " ", "Sierra", "Charlie", "Romeo", "Oscar", "Lima", "Lima",
-        "India", "November", "Golf", " "
-    ]
-
-    virtual = viewport(device, width=device.width, height=len(words) * 8)
-    with canvas(virtual) as draw:
-        for i, word in enumerate(words):
-            text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
-
-    for i in range(virtual.height - device.height):
-        virtual.set_position((0, i))
-        time.sleep(0.05)
-
-    msg = "Brightness"
-    print(msg)
-    show_message(device, msg, fill="white")
-
-    time.sleep(1)
-    with canvas(device) as draw:
-        text(draw, (0, 0), "A", fill="white")
-
-    time.sleep(1)
-    for _ in range(5):
-        for intensity in range(16):
-            device.contrast(intensity * 16)
-            time.sleep(0.1)
-
-    device.contrast(0x80)
-    time.sleep(1)
-
-    msg = "Alternative font!"
-    print(msg)
-    show_message(device, msg, fill="white", font=SINCLAIR_FONT)
-
-    time.sleep(1)
-    msg = "Proportional font - characters are squeezed together!"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(SINCLAIR_FONT))
-
-    # http://www.squaregear.net/fonts/tiny.shtml
-    time.sleep(1)
-    msg = "Tiny is, I believe, the smallest possible font \
-    (in pixel size). It stands at a lofty four pixels \
-    tall (five if you count descenders), yet it still \
-    contains all the printable ASCII characters."
-    msg = re.sub(" +", " ", msg)
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(TINY_FONT))
-
-    time.sleep(1)
-    msg = "CP437 Characters"
-    print(msg)
-    show_message(device, msg)
-
-    time.sleep(1)
-    for x in range(256):
-        with canvas(device) as draw:
-            text(draw, (0, 0), chr(x), fill="white")
-            time.sleep(0.1)
+    time.sleep(5)
 
 
 if __name__ == "__main__":
