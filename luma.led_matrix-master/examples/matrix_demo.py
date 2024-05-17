b9aba71e0dd2b915a776344a0d4991e8a204effd
TizianoHranov <Tiziano.Hranov@copadata.com>
2024-05-17 17:57:47 +0200

    forgot something

--- luma.led_matrix-master/examples/matrix_demo.py
+++ luma.led_matrix-master/examples/matrix_demo.py
@@ -20,7 +20,7 @@ def demo(n, block_orientation, rotate, inreverse):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
 
-    device = max7219(serial, cascaded=4, block_orientation=90, , blocks_arranged_in_reverse_order=True)
+    device = max7219(serial, cascaded=4, block_orientation=90, blocks_arranged_in_reverse_order=True)
 
     print("displaying test bitmap now")
 
