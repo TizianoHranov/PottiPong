2e5542efd22e36ad8dc74239027340002b0c4542
TizianoHranov <Tiziano.Hranov@copadata.com>
2024-05-17 19:10:03 +0200

    math is hard

--- c_matrix_demo.c
+++ c_matrix_demo.c
@@ -32,7 +32,7 @@ int main() {
         data[0][1] = 1;
         
         for (int i = 2; i < 32; i++) {
-            data[0][i] = data[0][i-1] + 2*data[0][i];  // 0x00, 0x01, 0x03, 0x07 .... 0xff
+            data[0][i] = data[0][i-1] + 2*data[0][i-1];  // 0x00, 0x01, 0x03, 0x07 .... 0xff
         }
 
 
